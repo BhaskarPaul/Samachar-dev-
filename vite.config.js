25a3af945217a0b792bf50a8529c370360ec79cf
Bhaskar Paul <paulbhaskar007@gmail.com>
2026-09-20 13:50:07 +0530

    Refactor API key handling to use process.env for environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, transformWithOxc } from "vite";
+import { defineConfig, transformWithOxc, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // Custom plugin to force Oxc to parse JSX inside .js files
@@ -17,24 +17,34 @@ const allowJsxInJs = () => ({
   },
 });
 
-export default defineConfig({
-  plugins: [
-    react(),
-    allowJsxInJs(), // Plugs directly into the Vite/Oxc pipeline
-  ],
-  base: '/samachar/',
-  // Add this legacy flag to handle strict CommonJS imports smoothly
-  legacy: {
-    inconsistentCjsInterop: true,
-  },
-  server: {
-    port: 3000,
-  },
-  optimizeDeps: {
-    rolldownOptions: {
-      moduleTypes: {
-        ".js": "jsx", // Ensures dependency-optimization handles third-party JS containing JSX
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const processEnvValues = Object.keys(env).reduce((prev, key) => {
+    prev[`process.env.${key}`] = JSON.stringify(env[key]);
+    return prev;
+  }, {});
+
+  return {
+    plugins: [
+      react(),
+      allowJsxInJs(), // Plugs directly into the Vite/Oxc pipeline
+    ],
+    base: "/samachar/",
+    define: processEnvValues,
+    // Add this legacy flag to handle strict CommonJS imports smoothly
+    legacy: {
+      inconsistentCjsInterop: true,
+    },
+    server: {
+      port: 3000,
+    },
+    optimizeDeps: {
+      rolldownOptions: {
+        moduleTypes: {
+          ".js": "jsx", // Ensures dependency-optimization handles third-party JS containing JSX
+        },
       },
     },
-  },
+  };
 });
